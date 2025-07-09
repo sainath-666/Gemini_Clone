30ab53c4bf19577cbfc393d01518c6486af54789
Sainathreddy <sai65265@gmail.com>
2025-07-10 02:01:19 +0530

    added featurs to sidebar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -17,15 +17,28 @@ const ContextProvided = (props) => {
     }, 75 * index);
   };
 
+
+  const newChat = () => {
+    setLoading(false)
+    setShowResult(false)
+  }
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    setPrevPrompts(prev=>[...prev,input])
-    const response = await runChat(input);
+    let response;
+    if (prompt !== undefined){
+      response = await runChat(prompt);
+      setRecentPrompt(prompt)
+    }
+    else {
+      setPrevPrompts(prev=>[...prev,input])
+      setRecentPrompt(input)
+      response = await runChat(input)
+    }
     let responseArray = response.split("**");
-    let newResponse;
+    let newResponse="";
     for (let i = 0; i < responseArray.length; i++) {
       if (i === 0 || i % 2 !== 1) {
         newResponse += responseArray[i];
@@ -54,6 +67,7 @@ const ContextProvided = (props) => {
     resultData,
     input,
     setInput,
+    newChat
   };
 
   return (
